429774e64d3a60dd27564682ebe5a93f57d63395
RecioJessieKirk <recio.jessiekirk@ue.edu.ph>
2026-05-16 14:09:23 +0800

    Added ascending and descending sort. Added 'clearable' for on year filter

--- app/composables/useLaunches.ts
+++ app/composables/useLaunches.ts
@@ -24,5 +24,46 @@ const query = gql`
 export const useLaunches = () => {
 	const { data } = useAsyncQuery<{ launches: Launches[] }>(query)
 	const launches = computed(() => data.value?.launches ?? [])
-	return { launches }
+
+	const selectedYear = ref('') // <- Filter state
+
+	const filteredLaunches = computed(() =>
+		launches.value.filter((m) =>
+			selectedYear.value
+				? new Date(m.launch_date_utc).getFullYear().toString() === selectedYear.value
+				: true,
+		),
+	)
+
+	const availableYears = computed(() =>
+		[...new Set(launches.value.map((l) => new Date(l.launch_date_utc).getFullYear().toString()))].sort(),
+	)
+
+	const sortOrder = ref<'asc' | 'desc'>('asc')
+	const sortedLaunch = computed(() => {
+		const data = [...filteredLaunches.value]
+		data.sort((a, b) => {
+			const modifier = sortOrder.value === 'asc' ? 1 : -1
+
+			const timeA = Date.parse(a.launch_date_utc)
+			const timeB = Date.parse(b.launch_date_utc)
+			return (timeA - timeB) * modifier
+		})
+		return data
+	})
+
+	// const sortOrder = ref<'asc' | 'desc'>('asc')
+
+	// const sortedLaunches = computed(() =>
+	// 	[...filteredLaunches.value].sort((a, b) => {
+	// 		const dateA = new Date(a.launch_date_utc).getTime()
+	// 		const dateB = new Date(b.launch_date_local).getTime()
+	// 		return sortOrder.value === 'asc' ? dateA - dateB : dateB - dateA
+	// 	}),
+	// )
+
+	return { filteredLaunches, selectedYear, availableYears, sortedLaunch, sortOrder }
+	// return { filteredLaunches, selectedYear, availableYears, sortedLaunches, sortOrder }
+
+	// return { launches }
 }
--- app/pages/launches-space-x.vue
+++ app/pages/launches-space-x.vue
@@ -2,7 +2,15 @@
 	<v-app class="space-bg-global-2">
 		<v-main>
 			<v-container>
-				<v-data-table :headers="headers" :items="launches">
+				<v-select v-model="selectedYear" :items="availableYears" clearable />
+				<div class="d-flex justify-end">
+					<v-btn-toggle v-model="sortOrder" mandatory>
+						<v-btn value="asc" variant="outlined">ASC</v-btn>
+						<v-btn value="dsc" variant="outlined">DSC</v-btn>
+					</v-btn-toggle>
+				</div>
+
+				<v-data-table :headers="headers" :items="sortedLaunch">
 					<template #[`item.launch_success`]="{ item }">
 						<v-chip :color="item.launch_success ? 'green' : 'red'" size="small">
 							{{ item.launch_success ? 'Success' : 'Failed' }}
@@ -14,12 +22,19 @@
 						</tr>
 					</template>
 				</v-data-table>
+				<!-- <div class="d-flex justify-end">
+					<v-btn-toggle v-model="sortOrder" mandatory>
+						<v-btn value="asc">ASC</v-btn>
+						<v-btn value="desc">DSC</v-btn>
+					</v-btn-toggle>
+				</div> -->
 			</v-container>
 		</v-main>
 	</v-app>
 </template>
 <script lang="ts" setup>
-const { launches } = await useLaunches()
+const { selectedYear, availableYears, sortedLaunch, sortOrder } = await useLaunches()
+// const { selectedYear, availableYears, filteredLaunches, sortedLaunches, sortOrder } = await useLaunches()
 
 const headers = [
 	{ title: 'Rocket Name', key: 'rocket.rocket_name' },
